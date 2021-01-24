a916a8c316c1a6807c8d7de4cb57830f3deaa8b0
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-01-24 12:42:26 -0700

    Add files via upload
    I make some modification

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CS133PROJECT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.MyCake<string>(" Vanilla ", " Wedding cake ");
+            myClass.MyCake2<string>(" Chocolate ", " Party cake ");
+
+        }
+    }
+}
+
+public class MyClass
+{
+    public void MyCake<C>(C myFlavor, string myType)
+    
+    {
+        Console.WriteLine("The Flavor of my cake is:" + myFlavor);
+        Console.WriteLine("The Type of my cake is for:" + myType);
+
+        Console.WriteLine("");
+    }
+
+    public void MyCake2<W>(W myFlavor, string myType)
+    {
+        Console.WriteLine("The Flavor of my second cake is:" + myFlavor);
+        Console.WriteLine("The Type of my  secondcake is for:" + myType);
+
+    }
+
+
+
+}
